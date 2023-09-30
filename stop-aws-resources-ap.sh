75a500c97ab4f40933eeeb7e51f7a88c36b2be30
Keegan Mullaney <kmullaney@newrelic.com>
2023-09-30 09:45:49 -0700

    feat: add ECS and CW log groups

--- stop-aws-resources-ap.sh
+++ stop-aws-resources-ap.sh
@@ -26,8 +26,12 @@ EXCLUDE_METRIC_STREAMS=()
 EXCLUDE_INSTANCES=()
 EXCLUDE_ECS_CLUSTERS=()
 EXCLUDE_EKS_CLUSTERS=()
+EXCLUDE_LOG_GROUPS=("/aws/lambda/aws-controltower-NotificationForwarder")
 
-# --------------------------  HELPER FUNCTIONS
+# CW log group retention
+RETENTION_PERIOD=3
+
+# --------------------------  TRAPS
 
 # Function to be executed when Ctrl+C is pressed
 handle_ctrl_c() {
@@ -39,6 +43,16 @@ handle_ctrl_c() {
 
 # Associate the function with the SIGINT signal (Ctrl+C)
 trap handle_ctrl_c SIGINT
+
+# Define the cleanup function
+cleanup() {
+  unset -f scale_down_ecs_clusters scale_down_eks_clusters stop_metric_streams stop_instances set_log_group_retention
+}
+
+# Set the trap to call the cleanup function on specific signals
+trap cleanup EXIT HUP INT TERM
+
+# --------------------------  HELPER FUNCTIONS
 
 lib-has() {
   type "$1" >/dev/null 2>&1
@@ -57,12 +71,6 @@ lib-checks() {
   if ! aws sts get-caller-identity >/dev/null; then exit 1; fi
 }
 
-# unset functions to free up memmory
-lib-unset() {
-  unset -f scale_down_eks_clusters stop_metric_streams stop_instances
-  exit $1
-}
-
 # output message with encoded characters
 # $1 -> string
 lib-msg() {
@@ -79,10 +87,9 @@ lib-thanks() {
   echo
   lib-version "$1"
   lib-msg "Made with <3 by Keegan Mullaney, a Lead Technical Support Engineer at New Relic."
-  lib-unset 0
 }
 
-is_excluded() {
+lib_is_excluded() {
   local resource=$1
   local -n exclusion_list=$2  # -n makes exclusion_list a reference to the array passed
   for exclude in "${exclusion_list[@]}"; do
@@ -91,66 +98,58 @@ is_excluded() {
   return 1
 }
 
-# --------------------------  FUNCTIONS
-
-stop_metric_streams() {
-  lib-checks "aws"
-  local region=$1
-  echo "Stopping Metric Streams in region $region"
-  for stream in $(aws cloudwatch list-metric-streams --region "$region" --query 'Entries[].Name' --output text --no-paginate); do
-    is_excluded "$stream" EXCLUDE_METRIC_STREAMS && continue
-    stream_state=$(aws cloudwatch get-metric-stream --region "$region" --name "$stream" --query 'State' --output text --no-paginate)
-    if [ "$stream_state" != "stopped" ]; then
-      echo "  -> stopping $stream"
-      aws cloudwatch stop-metric-streams --region "$region" --names "$stream"
-    fi
-  done
+lib_print_header() {
+  local service=${1}
+  local action=${2^^}
+  lib-msg "$service: $action"
 }
+
+# --------------------------  FUNCTIONS
 
 stop_instances() {
   lib-checks "aws"
   local region=$1
-  echo "Stopping EC2 instances in region $region"
+  lib_print_header "EC2 instances" "Stopping"
   # shellcheck disable=SC2016
-  for instance in $(aws ec2 describe-instances --region "$region" --query 'Reservations[].Instances[?State.Name==`running`].InstanceId' --output text --no-paginate); do
-    is_excluded "$instance" EXCLUDE_INSTANCES && continue
+  for instance in $(aws ec2 describe-instances --region "$region" --query 'Reservations[].Instances[?State.Name==`running`].InstanceId' --output text); do
+    lib_is_excluded "$instance" EXCLUDE_INSTANCES && continue
     echo "  -> stopping $instance"
     aws ec2 stop-instances --region "$region" --instance-ids "$instance" --output text --no-paginate
   done
 }
 
-# scale_down_ecs_clusters() {
-#   lib-checks "aws"
-#   local region=$1
-#   echo "Stopping ECS clusters in region $region"
-#   for cluster in $(aws ecs list-clusters --region "$region" --query 'clusterArns[]' --output text --no-paginate); do
-#     is_excluded "$cluster" EXCLUDE_ECS_CLUSTERS && continue
+scale_down_ecs_clusters() {
+  lib-checks "aws"
+  local region=$1
+  lib_print_header "ECS clusters" "Scaling down"
+  for cluster in $(aws ecs list-clusters --region "$region" --query 'clusterArns[]' --output text); do
+    lib_is_excluded "$cluster" EXCLUDE_ECS_CLUSTERS && continue
 
-#     # Update all services to have 0 desired count
-#     for service in $(aws ecs list-services --region "$region" --cluster "$cluster" --query 'serviceArns[]' --output text --no-paginate); do
-#       echo "  -> scaling $service in $cluster to 0"
-#       aws ecs update-service --region "$region" --cluster "$cluster" --service "$service" --desired-count 0
-#     done
+    # Update all services to have 0 desired count
+    for service in $(aws ecs list-services --region "$region" --cluster "$cluster" --query 'serviceArns[]' --output text); do
+      echo "  -> scaling $service in $cluster to 0"
+      aws ecs update-service --region "$region" --cluster "$cluster" --service "$service" --desired-count 0 --output text --no-paginate
+    done
 
-#     # Stop all running tasks
-#     for task in $(aws ecs list-tasks --region "$region" --cluster "$cluster" --query 'taskArns[]' --output text --no-paginate); do
-#       echo "  -> stopping task $task in $cluster"
-#       aws ecs stop-task --region "$region" --cluster "$cluster" --task "$task" --output text --no-paginate
-#     done
-#   done
-# }
+    # Stop all running tasks
+    for task in $(aws ecs list-tasks --region "$region" --cluster "$cluster" --query 'taskArns[]' --output text); do
+      echo "  -> stopping task $task in $cluster"
+      aws ecs stop-task --region "$region" --cluster "$cluster" --task "$task" --output text --no-paginate
+    done
+  done
+}
 
 scale_down_eks_clusters() {
   lib-checks "aws"
   lib-checks "eksctl"
   lib-checks "kubectl"
   local region=$1
-  echo "Scaling down EKS clusters in region $region"
-  for eks_cluster in $(aws eks list-clusters --region "$region" --query 'clusters[]' --output text --no-paginate); do
-    is_excluded "$eks_cluster" EXCLUDE_EKS_CLUSTERS && continue
+  lib_print_header "EKS clusters" "Scaling down"
+  for eks_cluster in $(aws eks list-clusters --region "$region" --query 'clusters[]' --output text); do
+    lib_is_excluded "$eks_cluster" EXCLUDE_EKS_CLUSTERS && continue
 
     # if the node groups were created with aws
-    aws_nodegroup_names=$(aws eks list-nodegroups --region "$region" --cluster-name "$eks_cluster" --query 'nodegroups[]' --output text --no-paginate)
+    aws_nodegroup_names=$(aws eks list-nodegroups --region "$region" --cluster-name "$eks_cluster" --query 'nodegroups[]' --output text)
     for nodegroup in $aws_nodegroup_names; do
       current_node_count=$(aws eks describe-nodegroup --region "$region" --cluster-name "$eks_cluster" --nodegroup-name "$nodegroup" --query 'nodegroup.scalingConfig.desiredSize')
       if [ "$current_node_count" != "0" ]; then
@@ -173,13 +172,42 @@ scale_down_eks_clusters() {
   done
 }
 
+stop_metric_streams() {
+  lib-checks "aws"
+  local region=$1
+  lib_print_header "CW Metric Streams" "Stopping"
+  for stream in $(aws cloudwatch list-metric-streams --region "$region" --query 'Entries[].Name' --output text); do
+    lib_is_excluded "$stream" EXCLUDE_METRIC_STREAMS && continue
+    stream_state=$(aws cloudwatch get-metric-stream --region "$region" --name "$stream" --query 'State' --output text)
+    if [ "$stream_state" != "stopped" ]; then
+      echo "  -> stopping $stream"
+      aws cloudwatch stop-metric-streams --region "$region" --names "$stream"
+    fi
+  done
+}
+
+set_log_group_retention() {
+  local region=$1
+  lib_print_header "CW log groups" "Setting retention"
+
+  # iterate through the log groups and call the set log group retention
+  while read -r log_group current_retention; do
+    if [ "$current_retention" != "$RETENTION_PERIOD" ]; then
+        lib_is_excluded "$log_group" EXCLUDE_LOG_GROUPS && continue
+        echo "  -> setting CW log group retention for $log_group to $RETENTION_PERIOD days"
+        aws logs put-retention-policy --region "$region" --log-group-name "$log_group" --retention-in-days $RETENTION_PERIOD --output text --no-paginate
+    fi
+  done <<< "$(aws logs describe-log-groups --region "$region" --query 'logGroups[*].[logGroupName, retentionInDays]' --output text)"
+}
+
 # --------------------------  MAIN EXECUTION
 
 for region in "${REGIONS[@]}"; do
   printf "\n==== Processing region %s ====\n" "$region"
-  # scale_down_ecs_clusters "$region"
+  scale_down_ecs_clusters "$region"
   scale_down_eks_clusters "$region"
   stop_metric_streams "$region"
+  set_log_group_retention "$region"
   stop_instances "$region"
 done
 
